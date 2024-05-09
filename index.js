291d426852f0a34cb3cb83bdea6705ffec985c10
paternostrox <pedro.paternostro@gmail.com>
2024-05-09 14:44:16 -0300

    exercise 3-4 done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
   { 
     "id": 1,
     "name": "Arto Hellas", 
@@ -50,6 +50,13 @@ app.get('/api/persons/:id', (request, response) => {
     response.json(person)
   else
     response.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
 })
 
 const port = 3000
